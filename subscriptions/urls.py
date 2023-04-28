5ee6fde218a16eff18004f6a302024c3f72141a5
ibouzidi <bouzidi.idris42@gmail.com>
2023-04-28 16:44:01 +0200

    Removed unnecessary line and urls.

--- subscriptions/urls.py
+++ subscriptions/urls.py
@@ -10,7 +10,6 @@ urlpatterns = [
     path('cancel/confirm/', views.CancelConfirmView.as_view(), name='CancelConfirmView'),
     path("success/", views.SuccessView.as_view(), name="SuccessView"),
     path("cancelled/", views.CancelView.as_view(), name="CancelView"),
-    path('plan/trial/confirm_checkout/', views.TrialPlanView.as_view(), name='TrialPlanView'),
     # path("cancel-subscription/", views.CancelSubscriptionView.as_view(), name="CancelSubscriptionView"),
     # path("change-subscription/", views.ChangeSubscriptionView.as_view(), name="ChangeSubscriptionView"),
     # path("invoices/", views.ListInvoiceView.as_view(), name="ListInvoiceView"),
