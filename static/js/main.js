970e7b4c2a1fec74454bce59f36173c2e11b9786
ibouzidi <bouzidi.idris42@gmail.com>
2023-06-08 11:53:40 +0200

    Enhance nav menu by using one header for home and member space.Fixed css and html.Enhance active class mapping with multiple keys.

--- static/js/main.js
+++ static/js/main.js
@@ -28,8 +28,8 @@
             class: 'mobile-nav d-lg-none'
         });
         $('body').append($mobile_nav);
-        $('body').prepend('<button type="button" class="mobile-nav-toggle d-lg-none"><i class="icofont-navigation-menu"></i></button>');
-        $('body').append('<div class="mobile-nav-overly"></div>');
+        $('#nav-header').prepend('<button type="button" class="mobile-nav-toggle d-lg-none"><i class="icofont-navigation-menu"></i></button>');
+        $('#nav-header').append('<div class="mobile-nav-overly"></div>');
 
         $(document).on('click', '.mobile-nav-toggle', function (e) {
             $('body').toggleClass('mobile-nav-active');
