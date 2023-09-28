33e3e7df041bb2b281b8d1da74839581129d05c4
ibouzidi <bouzidi.idris42@gmail.com>
2023-09-28 14:07:48 +0200

    Added and updated the webhook for subscription success, updated, deleted. Added a function that manage user's and groups for the webhook. Added active and inactive group for the members of a team.

--- subscriptions/views.py
+++ subscriptions/views.py
@@ -2,6 +2,7 @@ import datetime
 import hashlib
 import json
 from django.conf import settings
+from django.core.exceptions import ObjectDoesNotExist
 from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse
 from django.utils import timezone
@@ -17,6 +18,7 @@ import stripe
 from django.contrib.auth.models import Group
 from account.models import Account
 from dj_dastore.decorator import user_is_active_subscriber
+from team.models import RoleChoices, Team
 
 stripe.api_key = settings.STRIPE_TEST_SECRET_KEY
 
@@ -236,11 +238,15 @@ def move_user_to_group(user, old_group_name, new_group_name):
 
     try:
         old_group = Group.objects.get(name=old_group_name)
-        new_group = Group.objects.get(name=new_group_name)
         user.groups.remove(old_group)
+    except Group.DoesNotExist:
+        print(f"Old group does not exist: {old_group_name}")
+
+    try:
+        new_group = Group.objects.get(name=new_group_name)
         user.groups.add(new_group)
     except Group.DoesNotExist:
-        print(f"Group does not exist: {old_group_name} or {new_group_name}")
+        print(f"New group does not exist: {new_group_name}")
 
 
 # class CancelSubscriptionView(View):
@@ -292,6 +298,34 @@ def validate_portal_access(request, token):
     return render(request, 'account/account_billing.html')
 
 
+def manage_user_status_and_group(user, is_active, target_group_name,
+                                 source_group_name=None):
+    print(f"Managing user {user.id}...")
+    user.is_active = is_active
+    user.save()
+    print(f"User active status set to {is_active}")
+
+    target_group = Group.objects.get(name=target_group_name)
+    source_group = Group.objects.get(name=source_group_name) if source_group_name else None
+
+    # Remove from source group if present
+    if source_group:
+        if user.groups.filter(name=source_group_name).exists():
+            print(f"Removing user from {source_group_name}")
+            user.groups.remove(source_group)
+        else:
+            print(f"User not in {source_group_name}")
+
+    # Add to target group if not already present
+    if not user.groups.filter(name=target_group_name).exists():
+        print(f"Adding user to {target_group_name}")
+        user.groups.add(target_group)
+    else:
+        print(f"User already in {target_group_name}")
+
+    print(f"User groups: {user.groups.all()}")
+
+
 @webhooks.handler("payment_intent.succeeded")
 def payment_intent_succeeded_event_listener(event, **kwargs):
     invoice_id = event.data["object"]["invoice"]
@@ -308,43 +342,201 @@ def payment_intent_succeeded_event_listener(event, **kwargs):
                 print("user")
                 print(user)
                 if user:
-                    user.is_active = True
-                    # Retrieve the price (previously plan) and set the user's storage limit
+                    # Retrieve the price (previously plan)
+                    # and set the user's storage limit
                     plan_id = line['plan']['id']
                     print("plan_id")
                     print(plan_id)
                     price = get_object_or_404(Price, id=plan_id)
                     # Get storage_limit from Price's metadata
                     user.storage_limit = int(price.metadata[
-                        "storage_limit"])
+                                                 "storage_limit"])
                     test = price.metadata["storage_limit"]
                     print("test metadata ")
                     print(test)
                     # Add the user to the 'Active Subscribers' group
-                    group = Group.objects.get(name='g_active_subscribers')
-                    user.groups.add(group)
-                    user.save()
+                    manage_user_status_and_group(user, True,
+                                                 'g_active_subscribers',
+                                                 'g_inactive_subscribers')
+
     return
+
+
+@webhooks.handler("customer.subscription.updated")
+def subscription_updated_event_listener(event, **kwargs):
+    # Retrieve subscription and customer details from the event object
+    subscription_data = event.data["object"]
+    customer_id = subscription_data["customer"]
+
+    # Retrieve the Stripe customer
+    customer = stripe.Customer.retrieve(customer_id)
+
+    # Retrieve the user ID from the customer's metadata
+    user_id = customer.metadata.get("user_id")
+
+    # Fetch the corresponding user from your database
+    user_account = Account.objects.filter(id=user_id).first()
+
+    if user_account:
+        # Update the Account object's plan_id and storage_limit
+        # based on the new subscription details
+        new_plan_id = subscription_data["items"]["data"][0]["price"]["id"]
+        user_account.plan_id = new_plan_id
+
+        # Assuming you have price metadata that contains the new storage limit
+        new_storage_limit = stripe.Price.retrieve(new_plan_id).metadata.get(
+            "storage_limit")
+        if new_storage_limit:
+            user_account.storage_limit = int(new_storage_limit)
+
+        # # Reactivate the user account
+        # Move user to 'Active Subscribers' group
+        manage_user_status_and_group(user_account, True,
+                                     'g_active_subscribers',
+                                     'g_inactive_subscribers')
+
+        print(f"Subscription updated for user: {user_account.username}")
+
+        # If the user is a team leader, also activate the team members
+        if user_account.is_team_leader:
+            leader_teams = Team.objects.filter(
+                memberships__user=user_account,
+                memberships__role=RoleChoices.LEADER.value)
+
+            for team in leader_teams:
+                team_members = Account.objects.filter(
+                    teams=team).exclude(id=user_account.id)
+
+                for member in team_members:
+                    # Move team members to 'Active Subscribers' group
+                    manage_user_status_and_group(member, True,
+                                                 'g_active_team_member',
+                                                 'g_inactive_team_member')
+                    print(f"Subscription also reactivated "
+                          f"for team member: {member.username}")
+
+    else:
+        print("No corresponding user found in the database.")
 
 
 @webhooks.handler("customer.subscription.deleted")
 def subscription_cancelled_event_listener(event, **kwargs):
+    # Retrieve customer and subscription ID
     subscription_id = event.data["object"]["id"]
+    customer_id = event.data["object"]["customer"]
+
+    # Retrieve the Stripe customer
+    customer = stripe.Customer.retrieve(customer_id)
+
+    # Retrieve the user ID from the customer's metadata
+    user_id = customer.metadata.get("user_id")
+
     try:
-        subscription = Subscription.objects.get(id=subscription_id)
-        user = subscription.user
-        print("subscription.user")
-        print("subscription.user")
-        print("subscription.user")
-        print(user)
-        subscription.canceled_at = timezone.now()
-        subscription.status = "cancelled"
-        subscription.save()
+        # Fetch corresponding user based on metadata's user_id
+        user_account = Account.objects.get(id=user_id)
 
-        # Move user to 'Inactive Subscribers' group
-        move_user_to_group(user, 'g_active_subscribers',
-                           'g_inactive_subscribers')
+        if user_account:
+            # Move leader to 'Inactive Subscribers' group
+            manage_user_status_and_group(user_account, False,
+                                         'g_inactive_subscribers',
+                                         'g_active_subscribers')
+            print(f"Subscription cancelled for user "
+                  f"(Leader): {user_account.username}")
 
-    except Subscription.DoesNotExist:
-        print("Subscription does not exist in the database")
-    return
+            # If the user is a team leader, handle the team members
+            if user_account.is_team_leader:
+                # Retrieve all teams the user is leader of through
+                # the Membership model
+                leader_teams = Team.objects.filter(
+                    memberships__user=user_account,
+                    memberships__role=RoleChoices.LEADER.value)
+
+                for team in leader_teams:
+                    # Retrieve all team members
+                    team_members = Account.objects.filter(
+                        teams=team).exclude(id=user_account.id)
+
+                    # Loop through team members and set each
+                    # to inactive and move to the inactive group
+                    for member in team_members:
+                        manage_user_status_and_group(member, False,
+                                                     'g_inactive_team_member',
+                                                     'g_active_team_member')
+                        print(f"Subscription also cancelled "
+                              f"for team member: {member.username}")
+
+        else:
+            print("No corresponding user found in the database.")
+
+    except ObjectDoesNotExist:
+        print("No corresponding user found in the database.")
+    except Exception as e:
+        print(f"An error occurred: {str(e)}")
+# @webhooks.handler("customer.subscription.deleted")
+# def subscription_cancelled_event_listener(event, **kwargs):
+#     # Retrieve customer and subscription ID
+#     subscription_id = event.data["object"]["id"]
+#     customer_id = event.data["object"]["customer"]
+#
+#     # Retrieve the Stripe customer
+#     customer = stripe.Customer.retrieve(customer_id)
+#
+#     # Retrieve the user ID from the customer's metadata
+#     user_id = customer.metadata.get("user_id")
+#
+#     try:
+#         # Fetch corresponding user based on metadata's user_id
+#         user_account = Account.objects.filter(id=user_id).first()
+#
+#         if user_account:
+#             # Update your own Subscription model here if needed
+#             # For now, just demonstrating how to set the user's status to inactive
+#
+#             user_account.is_active = False  # Setting to inactive as an example
+#             user_account.save()
+#
+#             # Move user to 'Inactive Subscribers' group
+#             move_user_to_group(user_account, 'g_active_subscribers',
+#                                'g_inactive_subscribers')
+#
+#             print(f"Subscription cancelled for user: {user_account.username}")
+#         else:
+#             print("No corresponding user found in the database.")
+#     except Exception as e:
+#         print(f"An error occurred: {str(e)}")
+
+
+# @webhooks.handler("customer.subscription.updated")
+# def subscription_updated_event_listener(event, **kwargs):
+#     # Retrieve subscription and customer details from the event object
+#     subscription_data = event.data["object"]
+#     customer_id = subscription_data["customer"]
+#
+#     # Retrieve the Stripe customer
+#     customer = stripe.Customer.retrieve(customer_id)
+#
+#     # Retrieve the user ID from the customer's metadata
+#     user_id = customer.metadata.get("user_id")
+#
+#     # Fetch the corresponding user from your database
+#     user_account = Account.objects.filter(id=user_id).first()
+#
+#     if user_account:
+#         # Update the Account object's plan_id and storage_limit based on the new subscription details
+#         new_plan_id = subscription_data["items"]["data"][0]["price"]["id"]
+#         user_account.plan_id = new_plan_id
+#         # Assuming you have price metadata that contains the new storage limit
+#         new_storage_limit = stripe.Price.retrieve(new_plan_id).metadata.get(
+#             "storage_limit")
+#         if new_storage_limit:
+#             user_account.storage_limit = int(new_storage_limit)
+#         user_account.save()
+#
+#         print(f"Subscription updated for user: {user_account.username}")
+#     else:
+#         print("No corresponding user found in the database.")
+
+
+@webhooks.handler_all
+def handle_all(event, **kwargs):
+    print(f"Received event: {event.type}")
